22b993faef67ae02c331b24a7446574c1fd1041e
bikram <bikram@fedora.local>
2023-05-22 00:09:09 -0400

    Add capa tags to attribute

--- orchestration/logwatcher.py
+++ orchestration/logwatcher.py
@@ -12,6 +12,8 @@ from rich.logging import RichHandler
 logging.basicConfig(level=logging.INFO, filename='logwatcher.log', filemode='w')
 from pymisp import MISPEvent, MISPAttribute
 import base64
+import json
+import jmespath
 
 class LogWatcher():
 
@@ -26,7 +28,8 @@ class LogWatcher():
         misp_event_dict = self.misp_client.get_event(misp_event_id)['Event']
         self.misp_event_obj = MISPEvent()
         self.misp_event_obj.from_dict(**misp_event_dict)
-
+        self.sandbox_tag = 'enriched_via_sandboxs'
+    
     def check_new_log(self):
         current_files = set(os.listdir(self.LOGS_DIR))
         logging.info("Current Files: " + str(current_files))
@@ -45,15 +48,43 @@ class LogWatcher():
         for file in log_files:
             if file == 'pestr_log.txt':
                 self.process_pestr()
+            elif file == 'capa_log.json':
+                self.process_capa()
+
+    def process_capa(self):
+        logfile_name = 'capa_log.json'
+        file_content = self.get_log_file(logfile_name)
+        capa_dict = json.loads(file_content)
+        artifact_md5 = capa_dict['meta']['sample']['md5']
+        attribute = self.get_attribute_by_value(artifact_md5)
+
+        attack_ids = []
+        for rule_name in capa_dict['rules']:
+            attack_dict = capa_dict['rules'][rule_name]['meta']['attack']
+            if len(attack_dict) > 0:
+                attack_ids.extend(jmespath.search("[].id", attack_dict))
+        if attribute:
+            for attack_id in attack_ids:
+                attribute.add_tag(attack_id)
+            attribute.add_tag('capa')
+            self.misp_event_obj.add_attribute(**attribute)
+            self.misp_client.update_event(self.misp_event_obj)
+
 
     def process_pestr(self):
+        """
+        Run Strings through ioc-finder to detect any IOCs. 
+        Convert those IOCs into MISP attributes and attach to the MISP event.
+        """
         logfile_name = 'pestr_log.txt'
         logging.info("Processing {}".format(logfile_name))
         log_file = None
+        
+        # Read log file
         try:
             log_file = open(f'{self.LOGS_DIR}/{logfile_name}', 'r')
         except Exception as e:
-            log_file = open(f'{self.LOGS_DIR}/{logfile_name}', encoding='utf-16')
+            log_file = open(f'{self.LOGS_DIR}/{logfile_name}', encoding='utf-16')  # Since errors when dumped from Win
 
         file_content = log_file.read()
         file_content_bytes = file_content.encode("ascii")
@@ -74,15 +105,16 @@ class LogWatcher():
                 attribute = self.convert_to_attribute(misp_type_mappings[ioc_type], ioc, 'pestr')
                 self.misp_event_obj.add_attribute(**attribute)
 
+        # Upload PEStr file
         self.misp_event_obj.add_attribute(
             'attachment', 
             value=logfile_name,
             data=base64.b64encode(file_content_bytes) 
         )
-        self.misp_event_obj.add_tag('enriched_via_sandbox')
+        self.misp_event_obj.add_tag(self.sandbox_tag)
 
         ## Push the updated event to MISP
-        event_dict = self.misp_client.update_event(self.misp_event_obj)
+        self.misp_client.update_event(self.misp_event_obj)
 
     def convert_to_attribute(self, ioc_type, value, analysis_tool):
         logging.info('Converting {} to attribute'.format(value))
@@ -94,6 +126,29 @@ class LogWatcher():
         attribute.add_tag(analysis_tool)
         attribute.add_tag('suspicious')
         return attribute
+
+    def get_attribute_by_value(self, value):
+        # Retrieve the attribute from the search result
+        logging.info("Searching for {}".format(value))
+        search_result = self.misp_client.search(controller='attributes', value=value)
+        if 'Attribute' in search_result:
+            for attribute in search_result['Attribute']:
+                if attribute['value'] == value:
+                    logging.info(f"Attribute ID: {attribute['id']}")
+                    attribute_obj = MISPAttribute()
+                    attribute_obj.from_dict(**attribute)
+                    return attribute_obj
+        logging.info("Attribute {} not found.".format_map(value))
+        return None
+
+    def get_log_file(self, logfile_name):
+        try:
+            log_file = open(f'{self.LOGS_DIR}/{logfile_name}', 'rb')
+        except Exception as e:
+            log_file = open(f'{self.LOGS_DIR}/{logfile_name}', encoding='utf-16')
+        file_content = log_file.read()
+        log_file.close()
+        return file_content
 
     def run(self):
         logging.info("LogWatcher initiated...")
